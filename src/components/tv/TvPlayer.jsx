897daf2dcc61c8b4ce57e89e633ed07e8e304e0e
stexxer11 <rolandorar19@gmail.com>
2026-05-24 02:50:25 -0500

    youtube

--- src/components/tv/TvPlayer.jsx
+++ src/components/tv/TvPlayer.jsx
@@ -11,15 +11,15 @@ function TvPlayer({
   return (
     <div className="absolute inset-0 z-0 bg-black">
       <YouTube
-        key={currentSong.id}
         videoId={currentSong.youtube_id}
-        className="absolute inset-0 w-full h-full"
-        iframeClassName="absolute inset-0 w-full h-full"
+        className="absolute inset-0 w-full h-full pointer-events-none"
+        iframeClassName="absolute inset-0 w-full h-full pointer-events-none"
         opts={{
           width: "100%",
           height: "100%",
           playerVars: {
             autoplay: 1,
+            mute: 1,
             controls: 0,
             disablekb: 1,
             fs: 0,
@@ -28,6 +28,8 @@ function TvPlayer({
             playsinline: 1,
             rel: 0,
             start: 0,
+            enablejsapi: 1,
+            origin: window.location.origin,
           },
         }}
         onReady={onReady}
